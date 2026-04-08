de58ca77e7a67a44029d046f161c2aaa6abc08ac
Cristian Mestanza Ortiz <cristianmestanzaortiz870@gmail.com>
2026-04-08 08:48:23 -0500

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/dashboard/Dashboard';
 import Gmail from './pages/gmail/Gmail';
+import BecasComedor from './pages/becasComedor/BecasComedor';
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Routes>
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/gmail" element={<Gmail />} />
+        <Route path="/comedor" element={<BecasComedor />} />
       </Routes>
     </BrowserRouter>
   );
